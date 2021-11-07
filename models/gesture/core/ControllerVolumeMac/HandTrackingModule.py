c0bae94d143dc4070b8e16bf1365d150a3f69772
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2021-11-07 17:19:12 +0530

    Volume Control Advanced

--- models/gesture/core/ControllerVolumeMac/HandTrackingModule.py
+++ models/gesture/core/ControllerVolumeMac/HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class handDetector():
     def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
@@ -27,8 +28,11 @@ class handDetector():
         return img
 
     def findPosition(self, img, handNo = 0, draw = True):
+        xList = []
+        yList = []
+        bbox = []
 
-        lmList = []
+        self.lmList = []
 
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -37,10 +41,50 @@ class handDetector():
 
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
-                lmList.append([id, cx, cy])
+                xList.append(cx)
+                yList.append(cy)
+
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-        return lmList
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def findDistance(self, p1, p2, img, draw = True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2, = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        if draw:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def fingersUp(self):
+        fingers = []
+        self.tipIds = [4, 8, 12, 16, 20]
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+
 
 def main():
     pTime = 0
