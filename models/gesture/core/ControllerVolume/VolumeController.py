b6590617cbc3553703cc0333d4511160e4e2253c
Rishabh <bestatcricketinaparna@gmail.com>
2021-11-07 10:55:00 +0530

    Updates Dhanges

--- models/gesture/core/ControllerVolume/VolumeController.py
+++ models/gesture/core/ControllerVolume/VolumeController.py
@@ -89,4 +89,5 @@ while True:
 	# Render Data
 	# Render FPS on Screen
 	cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+	cv2.imshow("Img", img)
 	cv2.waitKey(1)
