0453aa7e3df1bb34ae110ef6cc4eb3ee3381f268
Shubham23999 <93310052+Shubham23999@users.noreply.github.com>
2021-11-12 20:49:00 +0530

    Update Calendar.py

--- main/core/packages/Calendar/Calendar.py
+++ main/core/packages/Calendar/Calendar.py
@@ -1,1 +1,16 @@
-""" Calendar """
+import calendar
+import time
+
+print("It can give the dates till the year 9999 only")
+
+yy = int(input("Enter The Year:-"))  
+mm = int(input("Enter The Month:-"))
+
+
+if mm<=12:
+    print("")
+
+else:
+    print("INVALID MONTH")
+
+time.sleep(1)
