66a37ac588f171cf99cde77e9aa6c034ce56a56f
SupTechRM.Rishabh <SupTechRM.bestatcricketinaparna@gmail.com>
2021-11-13 13:20:21 +0530

    Uploaded Speech Synthesizer

--- main/main.py
+++ main/main.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-from data.realtime_mic import takeCommand
+from data.speech.RealtimeMic import takeCommand
 from intents import *
-from data.SpeechSynthesis import speech_Speak
+from data.speech.RealtimeSpeech import speech_Speak
 import json
-=======
-from data.RealtimeMic import takeCommand
-from intents import intents
->>>>>>> 1e88bea48dce26eebd01da1610eadf65bdc6d3d8
 
 # Take Speech Recognition input and process it realtime running a main function
 # main function will take the query
@@ -25,11 +20,7 @@ name = data['name']
 file.close()
 
 while True:
-<<<<<<< HEAD
     speech_Speak("Welcome " + name + ". As I have already introduced myself, I am Seven. You can now as me for help.")
     user_input = takeCommand()
-=======
-    user_input = input("Enter: ")
->>>>>>> 1e88bea48dce26eebd01da1610eadf65bdc6d3d8
     user_input = user_input.lower()
     intents(user_input)
